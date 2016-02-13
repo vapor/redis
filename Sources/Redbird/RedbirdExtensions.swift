dd054ffd7d4ace9f452a0e616275787156868c35
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-02-13 23:52:50 +0100

    added a todo

---
+++ Sources/Redbird/RedbirdExtensions.swift
@@ -0,0 +1,10 @@
+//
+//  RedbirdExtensions.swift
+//  Redbird
+//
+//  Created by Honza Dvorsky on 2/13/16.
+//  Copyright © 2016 Honza Dvorsky. All rights reserved.
+//
+
+//TODO: https://github.com/czechboy0/Redbird/issues/4
+
