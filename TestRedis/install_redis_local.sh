c8be56e27a937499b3388dbd72e99f773b3cccfc
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-02-14 21:37:41 +0100

    pipe installation standard output to null

--- TestRedis/install_redis_local.sh
+++ TestRedis/install_redis_local.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -O http://download.redis.io/redis-stable.tar.gz
-tar xvzf redis-stable.tar.gz
+curl -O http://download.redis.io/redis-stable.tar.gz >/dev/null
+tar xvzf redis-stable.tar.gz >/dev/null
 cd redis-stable
 make
