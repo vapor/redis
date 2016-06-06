a001a26beaa88566c8a3705552f50f8cb98478d3
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-06-06 15:02:54 +0200

    Whoopsie

--- Package.swift
+++ Package.swift
@@ -3,11 +3,7 @@ import PackageDescription
 let package = Package(
   name: "Redbird",
   dependencies: [
-<<<<<<< HEAD
     .Package(url: "https://github.com/czechboy0/Socks.git", majorVersion: 0, minor: 4)
-=======
-    .Package(url: "https://github.com/czechboy0/Socks.git", majorVersion: 0, minor: 3)
->>>>>>> master
   ],
   exclude: [],
   targets: [
