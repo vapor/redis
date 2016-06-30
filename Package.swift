59d9e68cad365648a9bd0d8b77e8e957541a2c94
collinhundley <collinhundley@gmail.com>
2016-06-30 15:42:02 -0600

    Add example target back

--- Package.swift
+++ Package.swift
@@ -1,8 +1,20 @@
 import PackageDescription
 
 let package = Package(
-  name: "Redbird",
-  dependencies: [
-    .Package(url: "https://github.com/czechboy0/Socks.git", majorVersion: 0, minor: 4)
-  ]
+    name: "Redbird",
+    dependencies: [
+        .Package(url: "https://github.com/czechboy0/Socks.git", majorVersion: 0, minor: 4)
+    ],
+    exclude: [],
+    targets: [
+        Target(
+            name: "Redbird"
+        ),
+        Target(
+            name: "RedbirdExample",
+            dependencies: [
+                .Target(name: "Redbird")
+            ]
+        )
+    ]
 )
