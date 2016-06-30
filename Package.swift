69a586076e50db4c711f40c726ad80bf52543ac5
collinhundley <collinhundley@gmail.com>
2016-06-30 15:33:12 -0600

    Remove example target

--- Package.swift
+++ Package.swift
@@ -4,17 +4,5 @@ let package = Package(
   name: "Redbird",
   dependencies: [
     .Package(url: "https://github.com/czechboy0/Socks.git", majorVersion: 0, minor: 4)
-  ],
-  exclude: [],
-  targets: [
-    Target(
-      name: "Redbird"
-    ),
-    Target(
-      name: "RedbirdExample",
-      dependencies: [
-        .Target(name: "Redbird")
-      ]
-    )
   ]
 )
