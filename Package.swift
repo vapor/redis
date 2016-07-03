36744f20701df28843eaf777c2d8220884518ae0
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-07-03 15:22:26 +0200

    New Socks (#30)

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
   name: "Redbird",
   dependencies: [
-    .Package(url: "https://github.com/czechboy0/Socks.git", majorVersion: 0, minor: 4)
+    .Package(url: "https://github.com/czechboy0/Socks.git", majorVersion: 0, minor: 5)
   ],
   exclude: [],
   targets: [
