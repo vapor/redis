12d9999cbe033294c7c15d87276c39d842074deb
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-07-11 12:49:08 +0200

    Socks upgraded to 0.8 (#35)

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
   name: "Redbird",
   dependencies: [
-    .Package(url: "https://github.com/czechboy0/Socks.git", majorVersion: 0, minor: 5)
+    .Package(url: "https://github.com/czechboy0/Socks.git", majorVersion: 0, minor: 8)
   ],
   exclude: [],
   targets: [
