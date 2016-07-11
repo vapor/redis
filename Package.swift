30f9aec59107c682c446133335af3421bc5f2c97
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-07-11 13:09:30 +0200

    Simplified Package.swift (#36)
    * Socks upgraded to 0.8
    
    * simplified package.swift

--- Package.swift
+++ Package.swift
@@ -5,16 +5,8 @@ let package = Package(
   dependencies: [
     .Package(url: "https://github.com/czechboy0/Socks.git", majorVersion: 0, minor: 8)
   ],
-  exclude: [],
   targets: [
-    Target(
-      name: "Redbird"
-    ),
-    Target(
-      name: "RedbirdExample",
-      dependencies: [
-        .Target(name: "Redbird")
-      ]
-    )
+    Target(name: "Redbird"),
+    Target(name: "RedbirdExample", dependencies: ["Redbird"])
   ]
 )
