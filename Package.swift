b60d71b4c1cdea84dd5774d70a8c1ae1b9fb88e4
Tanner Nelson <me@tanner.xyz>
2016-09-11 19:22:18 -0400

    socks name update

--- Package.swift
+++ Package.swift
@@ -4,9 +4,12 @@ let package = Package(
 	name: "Redbird",
 	targets: [
 		Target(name: "Redbird"),
-		Target(name: "RedbirdExample", dependencies: ["Redbird"])
+		// Target(name: "RedbirdExample", dependencies: ["Redbird"])
 	],
 	dependencies: [
-		.Package(url: "https://github.com/czechboy0/Socks.git", majorVersion: 0, minor: 12)
+		.Package(url: "https://github.com/vapor/socks.git", majorVersion: 0, minor: 12)
+	],
+	exclude: [
+        "Sources/RedbirdExample"
 	]
 )
