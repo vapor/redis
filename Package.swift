8a041c5da8c36659364b48cb3fcf3a42d26bf72b
Logan Wright <logan.william.wright@gmail.com>
2016-10-20 12:05:19 -0400

    update socks

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
 		// Target(name: "RedbirdExample", dependencies: ["Redbird"])
 	],
 	dependencies: [
-		.Package(url: "https://github.com/vapor/socks.git", majorVersion: 0, minor: 12)
+		.Package(url: "https://github.com/vapor/socks.git", majorVersion: 1)
 	],
 	exclude: [
         "Sources/RedbirdExample"
