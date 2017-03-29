0343a20aa012a15df6997c65cd861477438fa5a5
Tanner <me@tanner.xyz>
2017-03-29 23:35:31 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 import PackageDescription
 
 let package = Package(
-	name: "Redis",
-	dependencies: [
+    name: "Redis",
+    dependencies: [
         // Core extensions, type-aliases, and functions that facilitate common tasks.
         .Package(url: "https://github.com/vapor/core.git", Version(2,0,0, prereleaseIdentifiers: ["beta"])),
 
@@ -10,9 +10,9 @@ let package = Package(
         .Package(url: "https://github.com/vapor/node.git", Version(2,0,0, prereleaseIdentifiers: ["beta"])),
 
         // Pure-Swift Sockets: TCP, UDP; Client, Server; Linux, OS X.
-		.Package(url: "https://github.com/vapor/socks.git", Version(2,0,0, prereleaseIdentifiers: ["beta"])),
+        .Package(url: "https://github.com/vapor/socks.git", Version(2,0,0, prereleaseIdentifiers: ["beta"])),
 		
-		// Module for generating random bytes and numbers (for tests).
+        // Module for generating random bytes and numbers (for tests).
         .Package(url: "https://github.com/vapor/random.git", majorVersion: 0)
-	]
+    ]
 )
