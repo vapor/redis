f6485a57d671be080a706874d452bf9dcd626335
John Bona <jbona88@gmail.com>
2017-05-18 11:14:48 -0400

    Support Vapor 2

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@ let package = Package(
     name: "Redis",
     dependencies: [
         // Core extensions, type-aliases, and functions that facilitate common tasks.
-        .Package(url: "https://github.com/vapor/core.git", Version(2,0,0, prereleaseIdentifiers: ["beta"])),
+        .Package(url: "https://github.com/vapor/core.git", majorVersion: 2),
 
         // A formatted data encapsulation meant to facilitate the transformation from one object to another.
-        .Package(url: "https://github.com/vapor/node.git", Version(2,0,0, prereleaseIdentifiers: ["beta"])),
+        .Package(url: "https://github.com/vapor/node.git", majorVersion: 2),
 
         // Pure-Swift Sockets: TCP, UDP; Client, Server; Linux, OS X.
-        .Package(url: "https://github.com/vapor/socks.git", Version(2,0,0, prereleaseIdentifiers: ["beta"])),
+        .Package(url: "https://github.com/vapor/sockets.git", majorVersion: 2),
 
         // Module for generating random bytes and numbers (for tests).
-        .Package(url: "https://github.com/vapor/random.git", Version(1,0,0, prereleaseIdentifiers: ["beta"]))
+        .Package(url: "https://github.com/vapor/random.git", majorVersion: 1)
     ]
 )
