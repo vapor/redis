14ea72e0f9263b2a744c469d47743cb351b6c120
John Bona <jbona88@gmail.com>
2017-11-30 02:04:31 -0500

    Implement support for SSL connections

--- Sources/Redis/Stream.swift
+++ Sources/Redis/Stream.swift
@@ -1,4 +1,5 @@
 import Sockets
+import TLS
 
 public var defaultHostname = "127.0.0.1"
 public var defaultPort: UInt16 = 6379
@@ -18,4 +19,25 @@ extension Client where StreamType == TCPInternetSocket {
         try socket.connect()
         try self.init(socket, password: password)
     }
+}
+
+public typealias TLSClient = Client<TLS.InternetSocket>
+
+extension Client where StreamType == TLS.InternetSocket {
+	public convenience init (
+		hostname: String = defaultHostname,
+		port: UInt16 = defaultPort,
+		password: String? = nil
+	) throws {
+		let socket = try TCPInternetSocket(
+			scheme: "rediss",
+			hostname: hostname,
+			port: port
+		)
+
+		let stream = try TLS.InternetSocket(socket, Context(.client))
+
+		try stream.connect()
+		try self.init(stream, password: password)
+	}
 }
