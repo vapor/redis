529687c0f3cc37afa0e34b8052caf60ce9bc339b
Shaun Hubbard <shaun@splife.com>
2018-02-18 12:03:40 -0500

    updating Package reqs to be friendlier towards betta progression

--- Package.swift
+++ Package.swift
@@ -3,25 +3,25 @@
 import PackageDescription
 
 let package = Package(
-    name: "Redis",
-    products: [
-        .library(name: "Redis", targets: ["Redis"]),
-    ],
-    dependencies: [
-        // Swift Promises, Futures, and Streams.
-        .package(url: "https://github.com/vapor/async.git", .exact("1.0.0-beta.1")),
+  name: "Redis",
+  products: [
+    .library(name: "Redis", targets: ["Redis"]),
+  ],
+  dependencies: [
+    // Swift Promises, Futures, and Streams.
+    .package(url: "https://github.com/vapor/async.git", from: "1.0.0-beta"),
 
-        // Core extensions, type-aliases, and functions that facilitate common tasks.
-        .package(url: "https://github.com/vapor/core.git", .exact("3.0.0-beta.1")),
+    // Core extensions, type-aliases, and functions that facilitate common tasks.
+    .package(url: "https://github.com/vapor/core.git", from: "3.0.0-beta"),
 
-        // Non-blocking networking for Swift.
-        .package(url: "https://github.com/vapor/sockets.git", .exact("3.0.0-beta.1")),
+    // Non-blocking networking for Swift.
+    .package(url: "https://github.com/vapor/sockets.git", from: "3.0.0-beta"),
 
-        // Core services for creating database integrations.
-        .package(url: "https://github.com/vapor/database-kit.git", .exact("1.0.0-beta.1")),
-    ],
-    targets: [
-        .target(name: "Redis", dependencies: ["Async", "Bits", "DatabaseKit", "Debugging", "TCP"]),
-        .testTarget(name: "RedisTests", dependencies: ["Redis"]),
-    ]
+    // Core services for creating database integrations.
+    .package(url: "https://github.com/vapor/database-kit.git", from: "1.0.0-beta"),
+  ],
+  targets: [
+    .target(name: "Redis", dependencies: ["Async", "Bits", "DatabaseKit", "Debugging", "TCP"]),
+    .testTarget(name: "RedisTests", dependencies: ["Redis"]),
+  ]
 )
