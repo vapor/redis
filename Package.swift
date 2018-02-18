ca6c870e86a209958163e309c028ae5b633d443c
Shaun Hubbard <shaun@splife.com>
2018-02-18 13:08:50 -0500

    change package tagging per recomendations from  community

--- Package.swift
+++ Package.swift
@@ -9,16 +9,16 @@ let package = Package(
   ],
   dependencies: [
     // Swift Promises, Futures, and Streams.
-    .package(url: "https://github.com/vapor/async.git", from: "1.0.0-beta"),
+    .package(url: "https://github.com/vapor/async.git",  "1.0.0-beta.1"..<"1.0.0-beta.1.1"),
 
     // Core extensions, type-aliases, and functions that facilitate common tasks.
-    .package(url: "https://github.com/vapor/core.git", from: "3.0.0-beta"),
+    .package(url: "https://github.com/vapor/core.git", "3.0.0-beta.1"..<"3.0.0-beta.1.1"),
 
     // Non-blocking networking for Swift.
-    .package(url: "https://github.com/vapor/sockets.git", from: "3.0.0-beta"),
+    .package(url: "https://github.com/vapor/sockets.git", "3.0.0-beta.1"..<"3.0.0-beta.2.1"),
 
     // Core services for creating database integrations.
-    .package(url: "https://github.com/vapor/database-kit.git", from: "1.0.0-beta"),
+    .package(url: "https://github.com/vapor/database-kit.git", "1.0.0-beta.1"..<"1.0.0-beta.3"),
   ],
   targets: [
     .target(name: "Redis", dependencies: ["Async", "Bits", "DatabaseKit", "Debugging", "TCP"]),
