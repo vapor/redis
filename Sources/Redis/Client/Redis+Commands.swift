71913b361d4de7d59acc4529bce8e16e06004b60
John Connolly <connoljo2@gmail.com>
2018-02-28 16:12:41 -0400

    Added more tests and delete command

--- Sources/Redis/Client/Redis+Commands.swift
+++ Sources/Redis/Client/Redis+Commands.swift
@@ -1,12 +1,24 @@
 import Foundation
 import Async
 
+/// Connection commands
 extension RedisClient {
-    
+    /// Request for authentication in a password-protected Redis server
     public func authorize(with password: String) -> Future<Void> {
         return command("AUTH", [RedisData(bulk: password)]).transform(to: ())
     }
     
+}
+
+/// Key commands
+extension RedisClient {
+    /// Removes the specified keys. A key is ignored if it does not exist.
+    public func delete(_ keys: [String]) throws -> Future<Int> {
+        let resp = command("DEL", keys.map(RedisData.init(bulk:))).map(to: Int?.self) { data in
+            return data.int
+        }
+        return resp.unwrap(or: RedisError(identifier: "delete", reason: "Could not convert resp to int", source: .capture()))
+    }
 }
 
 /// String commands
--- Tests/RedisTests/RedisTests.swift
+++ Tests/RedisTests/RedisTests.swift
@@ -15,36 +15,36 @@ class RedisTests: XCTestCase {
         XCTAssertEqual(get, "world")
         try redis.remove("hello").await(on: eventLoop)
     }
-
+    
     func testPubSub() throws {
         // Setup
         let eventLoop = try DefaultEventLoop(label: "codes.vapor.redis.test.pubsub")
         let promise = Promise(RedisData.self)
-
+        
         // Subscribe
         try RedisClient.subscribe(to: ["foo"], on: eventLoop) { _, error in
             XCTFail("\(error)")
-        }.await(on: eventLoop).drain { data in
-            XCTAssertEqual(data.channel, "foo")
-            promise.complete(data.data)
-        }.catch { error in
-            XCTFail("\(error)")
-        }.finally {
-            // closed
+            }.await(on: eventLoop).drain { data in
+                XCTAssertEqual(data.channel, "foo")
+                promise.complete(data.data)
+            }.catch { error in
+                XCTFail("\(error)")
+            }.finally {
+                // closed
         }
-
+        
         // Publish
         let publisher = try RedisClient.connect(on: eventLoop) { _, error in
             XCTFail("\(error)")
         }
         let publish = try publisher.publish(.bulkString("it worked"), to: "foo").await(on: eventLoop)
         XCTAssertEqual(publish.int, 1)
-
+        
         // Verify
         let data = try promise.future.await(on: eventLoop)
         XCTAssertEqual(data.string, "it worked")
     }
-
+    
     func testStruct() throws {
         struct Hello: Codable {
             var message: String
@@ -64,7 +64,7 @@ class RedisTests: XCTestCase {
         XCTAssertEqual(get?.dict["yes"], true)
         XCTAssertEqual(get?.dict["false"], false)
         try redis.remove("hello").await(on: eventLoop)
-
+        
     }
     
     func testStringCommands() throws {
@@ -78,15 +78,26 @@ class RedisTests: XCTestCase {
         let resp = try redis.mget(["hello", "hello2"]).await(on: eventLoop)
         XCTAssertEqual(resp[0].string, "world")
         XCTAssertEqual(resp[1].string, "world2")
-        try redis.remove("hello").await(on: eventLoop)
-        try redis.remove("hello2").await(on: eventLoop)
+        _ = try redis.delete(["hello", "hello2"]).await(on: eventLoop)
+        
+        
+        let number = try redis.increment("number").await(on: eventLoop)
+        XCTAssertEqual(number, 1)
+        let number2 = try redis.increment("number", by: 10).await(on: eventLoop)
+        XCTAssertEqual(number2, 11)
+        let number3 = try redis.decrement("number", by: 10).await(on: eventLoop)
+        XCTAssertEqual(number3, 1)
+        let number4 = try redis.decrement("number").await(on: eventLoop)
+        XCTAssertEqual(number4, 0)
+        _ = try redis.delete(["number"]).await(on: eventLoop)
+        
         
     }
-
+    
     static let allTests = [
         ("testCRUD", testCRUD),
         ("testPubSub", testPubSub),
         ("testStruct", testStruct),
         ("testStringCommands", testStringCommands),
-    ]
+        ]
 }
