739958a3186fda2b76f8e435a26fd49c55c10c7d
John Connolly <connoljo2@gmail.com>
2018-03-31 12:14:35 -0300

    Fixed bug with makeConnection

--- Sources/Redis/Database/RedisDatabase.swift
+++ Sources/Redis/Database/RedisDatabase.swift
@@ -3,7 +3,7 @@ import DatabaseKit
 import Async
 
 public final class RedisDatabase: Database {
-    /// ReidsDatabase has connection to RedisClient because it is more of a dataStructure store and less
+    /// RedisDatabase has connection to RedisClient because it is more of a dataStructure store and less
     /// of a first class ORM
     public typealias Connection = RedisClient
 
@@ -16,15 +16,22 @@ public final class RedisDatabase: Database {
     }
 
     public func makeConnection(on worker: Worker) -> EventLoopFuture<RedisClient> {
-        return RedisClient.connect(hostname: config.hostname, port: config.port, on: worker) { error in
+        let client = RedisClient.connect(hostname: config.hostname, port: config.port, on: worker) { error in
             print("[Redis] \(error)")
-        }.map(to: RedisClient.self, { client in
+        }
+        let promise = worker.eventLoop.newPromise(RedisClient.self)
+        client.do { client in
             if let password = self.config.password {
-                _ = client.command("AUTH", [.basicString(password)])
+                client.authorize(with: password).do {
+                    promise.succeed(result: client)
+                }.catch(promise.fail(error:))
+            } else {
+                promise.succeed(result: client)
             }
-            return client
-        })
+        }.catch(promise.fail(error:))
+        return promise.futureResult
     }
+
 }
 
 extension RedisClient: DatabaseConnection, BasicWorker { }
