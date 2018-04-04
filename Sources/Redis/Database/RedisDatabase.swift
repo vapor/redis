47c1e925610e54b9d6057dd5ef2c31d100b98de5
Shaun Hubbard <shaun@splife.com>
2018-04-03 21:24:04 -0400

    added urlConfig per @tanner1010

--- Sources/Redis/Database/RedisDatabase.swift
+++ Sources/Redis/Database/RedisDatabase.swift
@@ -8,18 +8,22 @@ public final class RedisDatabase: Database {
     public typealias Connection = RedisClient
 
     /// This client's configuration.
-    public let config: RedisClientConfig
+    public let url: URL
 
     /// Creates a new `RedisDatabase`.
-    public init(config: RedisClientConfig) {
-        self.config = config
+    public init(config: RedisClientConfig) throws {
+        url = try config.toURL()
+    }
+
+    public init(url: URL) throws {
+        self.url = url
     }
 
     public func makeConnection(on worker: Worker) -> EventLoopFuture<RedisClient> {
-        return RedisClient.connect(hostname: config.hostname, port: config.port, on: worker) { error in
+        return RedisClient.connect(hostname: url.host ?? "localhost", port: url.port ?? 6379, on: worker) { error in
             print("[Redis] \(error)")
         }.map(to: RedisClient.self, { client in
-            if let password = self.config.password {
+            if let password = self.url.password {
                 _ = client.command("AUTH", [.basicString(password)])
             }
             return client
