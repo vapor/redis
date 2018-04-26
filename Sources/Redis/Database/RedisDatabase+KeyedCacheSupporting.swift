cfb626d264ad6cb7a346ae0ff6f51f9ec15929c2
Shaun Hubbard <shaun@splife.com>
2018-04-26 16:46:31 -0400

    d'oh

--- Sources/Redis/Database/RedisDatabase+KeyedCacheSupporting.swift
+++ Sources/Redis/Database/RedisDatabase+KeyedCacheSupporting.swift
@@ -3,17 +3,11 @@ import DatabaseKit
 
 extension RedisDatabase: KeyedCacheSupporting {
     public static func keyedCacheGet<D>(_ key: String, as decodable: D.Type, on conn: RedisClient) throws -> EventLoopFuture<D?> where D : Decodable {
-        return conn.get(key, as: Data.self).map(to: D?.self) { data in
-            guard let data = data, data.count > 0 else { return nil }
-            let decoder = JSONDecoder()
-            return try decoder.decode(decodable, from: data)
-        }
+        return conn.jsonGet(key, as: decodable)
     }
 
     public static func keyedCacheSet<E>(_ key: String, to encodable: E, on conn: RedisClient) throws -> EventLoopFuture<Void> where E : Encodable {
-        let encoder = JSONEncoder()
-        let data = try encoder.encode(encodable)
-        return conn.set(key, to: data)
+        return conn.jsonSet(key, to: encodable)
     }
 
     public static func keyedCacheRemove(_ key: String, on conn: RedisClient) throws -> EventLoopFuture<Void> {
