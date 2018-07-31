65c7d3514d7fd55ac0a4c8d7053b644a15e9cc57
Shaun Hubbard <shaun@splife.com>
2018-07-30 22:51:45 -0400

    bumped versions

--- Package.swift
+++ Package.swift
@@ -7,14 +7,11 @@ let package = Package(
         .library(name: "Redis", targets: ["Redis"])
     ],
     dependencies: [
-        // Core extensions, type-aliases, and functions that facilitate common tasks.
-      .package(url: "https://github.com/vapor/core.git", from: "3.0.0-rc.2"),
+      // 🌎 Utility package containing tools for byte manipulation, Codable, OS APIs, and debugging.
+      .package(url: "https://github.com/vapor/core.git", from: "3.0.0"),
 
         // Core services for creating database integrations.
-        .package(url: "https://github.com/vapor/database-kit.git", from: "1.0.0"),
-
-        // Event-driven network application framework for high performance protocol servers & clients, non-blocking.
-        .package(url: "https://github.com/apple/swift-nio.git", from: "1.0.0")
+        .package(url: "https://github.com/vapor/database-kit.git", from: "1.0.0")
     ],
     targets: [
       .target(name: "Redis", dependencies: ["Async", "Bits", "DatabaseKit", "Debugging", "NIO", "COperatingSystem"]),
