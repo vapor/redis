05b0dd004fb35a57aa3ec0a5db6fbade06f03141
Andrei Popa <a.popa@cardiologiecentra.nl>
2018-11-09 09:37:11 +0100

    implemented & added tests for hash operations HSET, HKEYS, HGET, HDEL

--- Sources/Redis/Client/Redis+Commands.swift
+++ Sources/Redis/Client/Redis+Commands.swift
@@ -242,3 +242,97 @@ extension RedisClient {
         }
     }
 }
+
+
+/// Hash commands
+extension RedisClient {
+    
+    
+    /// Returns all field names in the hash stored at key.
+    /// - Returns: Array reply: list of fields in the hash, or an empty list when key does not exist.
+    public func hkeys(_ key: String) -> Future<[String]> {
+        // HKEYS key
+        let args = [RedisData(bulk: key)]
+        // first get array of fields
+        return command("HKEYS", args)
+            .map(to: [RedisData].self, { data in
+                guard let array = data.array else {
+                    throw RedisError(identifier: "hkeys", reason: "Could not convert resp to array.")
+                }
+                return array
+            })
+            // then convert to strings
+            .map({ array in
+                try array.map({ try String.convertFromRedisData($0) })
+            })
+    }
+    
+    /// Returns the value associated with field in the hash stored at key as a `RedisDataConvertible` type.
+    /// - Returns: the value associated with field, or nil when field is not present in the hash or key does not exist.
+    public func hget<D>(_ key: String, field: String, as type: D.Type) -> Future<D?> where D: RedisDataConvertible {
+        return hget(key, field: field).map(to: D?.self) { data in
+            if data.isNull {
+                return nil
+            } else {
+                return try D.convertFromRedisData(data)
+            }
+        }
+    }
+    
+    /// Returns the value associated with field in the hash stored at key.
+    /// - Returns: the value associated with field, or nil when field is not present in the hash or key does not exist.
+    public func hget(_ key: String, field: String) -> Future<RedisData> {
+        // HGET key field
+        let args = [RedisData(bulk: key), RedisData(bulk: field)]
+        return command("HGET", args)
+    }
+    
+    
+    /// Sets field in the hash stored at key to a value. If key does not exist, a new key holding a hash is created. If field already exists in the hash, it is overwritten.
+    /// - Returns: 1 if field is a new field in the hash and value was set, 0 if field already exists in the hash and the value was updated.
+    public func hset<E>(_ key: String, field: String, to data: E) -> Future<Int> where E: RedisDataConvertible {
+        do {
+            let data = try data.convertToRedisData()
+            return hset(key, field: field, to: data)
+        } catch {
+            return eventLoop.newFailedFuture(error: error)
+        }
+    }
+    
+    /// Sets field in the hash stored at key to a `RedisData` type. If key does not exist, a new key holding a hash is created. If field already exists in the hash, it is overwritten.
+    /// - Returns: 1 if field is a new field in the hash and value was set, 0 if field already exists in the hash and the value was updated.
+    public func hset(_ key: String, field: String, to data: RedisData) -> Future<Int> {
+        // HSET key field value
+        let args = [RedisData(bulk: key), RedisData(bulk: field), data]
+        return command("HSET", args).map(to: Int.self) { data in
+            guard let value = data.int else {
+                throw RedisError(identifier: "hset", reason: "Could not convert resp to int.")
+            }
+            return value
+        }
+    }
+    
+    
+    /// Removes the specified fields from the hash stored at key. Specified fields that do not exist within this hash are ignored.
+    public func hdel(_ key: String, fields: String...) -> Future<Int> {
+        // HDEL key field [field ...]
+        return hdel(key, fields: fields)
+    }
+    
+    /// Removes the specified fields from the hash stored at key. Specified fields that do not exist within this hash are ignored.
+    /// - Returns: Integer reply: the number of fields that were removed from the hash, not including specified but non existing fields. If key does not exist, it is treated as an empty hash and this command returns 0.
+    public func hdel(_ key: String, fields: [String]) -> Future<Int> {
+        // HDEL key field [field ...]
+        var args = [RedisData(bulk: key)]
+        args.append(contentsOf: fields.map(RedisData.init(bulk:)))
+        
+        let resp = command("HDEL", args).map(to: Int.self) { data in
+            guard let value = data.int else {
+                throw RedisError(identifier: "hdel", reason: "Could not convert resp to int.")
+            }
+            return value
+        }
+        return resp
+    }
+    
+}
