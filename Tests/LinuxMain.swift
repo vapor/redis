a09b993fc0c648c35dbf64059461b018c6d15cf9
Nathan Harris <nathan.harris@globant.com>
2019-01-04 23:39:26 -0800

    Add unit test cases to `LinuxMain.swift`

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -2,5 +2,9 @@ import XCTest
 @testable import RedisTests
 
 XCTMain([
-	testCase(RedisTests.allTests)
+    testCase(RedisTests.allTests),
+    testCase(RedisDatabaseTests.allTests),
+    testCase(RedisDataEncoderTests.allTests),
+    testCase(RedisDataDecoderTests.allTests),
+    testCase(RedisPipelineTests.allTests)
 ])
