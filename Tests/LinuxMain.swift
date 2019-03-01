aa12323090219c4875dfe5d8b2fb327774b15ee7
tanner0101 <me@tanner.xyz>
2019-02-28 22:42:02 -0500

    fix linux main

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -3,8 +3,4 @@ import XCTest
 
 XCTMain([
 	testCase(RedisTests.allTests),
-    testCase(RedisDataDecoderTests.allTests),
-    testCase(RedisDataEncoderTests.allTests),
-    testCase(RedisDatabaseTests.allTests),
-    testCase(RedisDataConvertibleTests.allTests)
 ])
