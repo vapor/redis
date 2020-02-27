0429f68c1ebaec1d83e761945b5e249a8e7ea1b4
Gwynne Raskind <gwynne@darkrainfall.org>
2020-02-27 05:17:55 -0600

    Match Vapor's minimum OS requirement and bump minimum required versions of dependencies. (#160)

--- Package.swift
+++ Package.swift
@@ -4,14 +4,14 @@ import PackageDescription
 let package = Package(
     name: "redis",
     platforms: [
-        .macOS(.v10_14)
+        .macOS(.v10_15)
     ],
     products: [
         .library(name: "Redis", targets: ["Redis"])
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/redis-kit.git", from: "1.0.0-beta.2"),
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta.2.1"),
+        .package(url: "https://github.com/vapor/redis-kit.git", from: "1.0.0-beta.3"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta.4"),
     ],
     targets: [
       .target(name: "Redis", dependencies: ["RedisKit", "Vapor"]),
