bdd82851725aff14cd1d28c04d34dfe26ac70cf0
Daniel Ramteke <danramteke@users.noreply.github.com>
2021-01-27 02:36:38 -0500

    Support for Multiple Redis instances (#178)
    Co-authored-by: Gwynne Raskind <gwynne@darkrainfall.org>
    Co-authored-by: Daniel <daniel@danramteke.com>
    Co-authored-by: Daniel R <daniel@macbookpro.local>

---
+++ Sources/Redis/Application.Redis+PubSub.swift
@@ -0,0 +1,28 @@
+import Vapor
+
+extension Application.Redis {
+    private struct PubSubKey: StorageKey, LockKey {
+        typealias Value = [RedisID: RedisClient]
+    }
+
+    var pubsubClient: RedisClient {
+        if let existing = self.application.storage[PubSubKey.self]?[self.id] {
+            return existing
+        } else {
+            let lock = self.application.locks.lock(for: PubSubKey.self)
+            lock.lock()
+            defer { lock.unlock() }
+
+            let pool = self.pool(for: self.eventLoop.next())
+
+            if let existingStorage = self.application.storage[PubSubKey.self] {
+                var copy = existingStorage
+                copy[self.id] = pool
+                self.application.storage.set(PubSubKey.self, to: copy)
+            } else {
+                self.application.storage.set(PubSubKey.self, to: [self.id: pool])
+            }
+            return pool
+        }
+    }
+}
--- Sources/Redis/Request+Redis.swift
+++ Sources/Redis/Request+Redis.swift
@@ -1,12 +1,14 @@
 import Vapor
 
 extension Request {
-    public var redis: Redis {
-        .init(request: self)
-    }
-
     public struct Redis {
+        public let id: RedisID
         let request: Request
+
+        init(request: Request, id: RedisID) {
+            self.request = request
+            self.id = id
+        }
     }
 }
 
@@ -16,13 +18,13 @@ extension Request.Redis: RedisClient {
     }
 
     public func logging(to logger: Logger) -> RedisClient {
-        self.request.application.redis
+        self.request.application.redis(self.id)
             .pool(for: self.eventLoop)
             .logging(to: logger)
     }
 
     public func send(command: String, with arguments: [RESPValue]) -> EventLoopFuture<RESPValue> {
-        self.request.application.redis
+        self.request.application.redis(self.id)
             .pool(for: self.eventLoop)
             .logging(to: self.request.logger)
             .send(command: command, with: arguments)
@@ -34,14 +36,14 @@ extension Request.Redis: RedisClient {
         onSubscribe subscribeHandler: RedisSubscriptionChangeHandler?,
         onUnsubscribe unsubscribeHandler: RedisSubscriptionChangeHandler?
     ) -> EventLoopFuture<Void> {
-        return self.request.application.redis
+        self.request.application.redis(self.id)
             .pubsubClient
             .logging(to: self.request.logger)
             .subscribe(to: channels, messageReceiver: receiver, onSubscribe: subscribeHandler, onUnsubscribe: unsubscribeHandler)
     }
     
     public func unsubscribe(from channels: [RedisChannelName]) -> EventLoopFuture<Void> {
-        return self.request.application.redis
+        self.request.application.redis(self.id)
             .pubsubClient
             .logging(to: self.request.logger)
             .unsubscribe(from: channels)
@@ -53,14 +55,14 @@ extension Request.Redis: RedisClient {
         onSubscribe subscribeHandler: RedisSubscriptionChangeHandler?,
         onUnsubscribe unsubscribeHandler: RedisSubscriptionChangeHandler?
     ) -> EventLoopFuture<Void> {
-        return self.request.application.redis
+        self.request.application.redis(self.id)
             .pubsubClient
             .logging(to: self.request.logger)
             .psubscribe(to: patterns, messageReceiver: receiver, onSubscribe: subscribeHandler, onUnsubscribe: unsubscribeHandler)
     }
     
     public func punsubscribe(from patterns: [String]) -> EventLoopFuture<Void> {
-        return self.request.application.redis
+        self.request.application.redis(self.id)
             .pubsubClient
             .logging(to: self.request.logger)
             .punsubscribe(from: patterns)
