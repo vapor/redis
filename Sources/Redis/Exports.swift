6d360de56d8635ec8f902bad9807cbdd255d1820
Tim <0xtimc@gmail.com>
2023-02-19 03:52:39 +0000

    More exports

--- Sources/Redis/Exports.swift
+++ Sources/Redis/Exports.swift
@@ -1,5 +1,8 @@
 #if !BUILDING_DOCC
 
-//@_exported import RediStack
+@_exported import RediStack
+@_exported import struct Foundation.URL
+@_exported import struct Logging.Logger
+@_exported import struct NIO.TimeAmount
 
 #endif
