210913b67894c445349bd477ae443b50ee709d32
Gwynne Raskind <gwynne@vapor.codes>
2023-05-27 11:53:43 -0500

    Update Exports.swift

--- Sources/Redis/Exports.swift
+++ Sources/Redis/Exports.swift
@@ -1,8 +1,15 @@
-#if !BUILDING_DOCC
+#if swift(>=5.8)
+
+@_documentation(visibility: internal) @_exported import RediStack
+@_documentation(visibility: internal) @_exported import struct Foundation.URL
+@_documentation(visibility: internal) @_exported import struct Logging.Logger
+@_documentation(visibility: internal) @_exported import struct NIOCore.TimeAmount
+
+#else
 
 @_exported import RediStack
 @_exported import struct Foundation.URL
 @_exported import struct Logging.Logger
-@_exported import struct NIO.TimeAmount
+@_exported import struct NIOCore.TimeAmount
 
 #endif
