42ca76f9d9ff2470aed60595ce9d53d228aeb067
Gwynne Raskind <gwynne@vapor.codes>
2023-06-20 10:13:42 -0500

    Correct RediStack URL and match Vapor's min Swift version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5.2
+// swift-tools-version:5.6
 import PackageDescription
 
 let package = Package(
@@ -13,7 +13,7 @@ let package = Package(
         .library(name: "Redis", targets: ["Redis"])
     ],
     dependencies: [
-        .package(url: "https://gitlab.com/swift-server-community/RediStack.git", from: "1.4.1"),
+        .package(url: "https://github.com/swift-server/RediStack.git", from: "1.4.1"),
         .package(url: "https://github.com/vapor/vapor.git", from: "4.50.0"),
     ],
     targets: [
